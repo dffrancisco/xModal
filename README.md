853944457194801901ee6c6e709dbf4a5db98708
dffrancisco <dffrancisco@gmail.com>
2020-04-09 10:33:51 -0300

    create xModal

---
+++ README.md
@@ -0,0 +1,307 @@
+# xModal v2.0
+
+O xModal é uma janela de diálogo personalizada inspirada inicialmente no showModal do Delphi. 
+Ele usa nativamente javascript em sua construção.
+
+[Instalação](#instalação)<br>
+[Importando](#importando)<br>
+[Modo de Uso](#modo-de-uso)<br>
+[Temas](#temas)<br>
+[Parâmetro Create](#parâmetro-create)<br>
+[Métodos](#métodos)<br>
+[Desenvolvedores](#desenvolvedores)<br>
+
+## Instalação
+
+Clone o repositório em um diretório local.
+
+```sh
+    git clone https://github.com/dffrancisco/xModal
+```
+
+## Importando
+
+<h3>Forma 1</h3>
+
+Em seu html declare o css:
+```html
+    <link rel="stylesheet" href="./xModalV2.css">
+```
+
+Para usar o xModal você deve chamá-lo em seu arquivo .js através de import.
+```javascript
+    import xModal from './xModalV2';
+```
+
+<h3>Forma 2</h3>
+
+Em seu html declare o arquivo js.
+
+```html
+    <link rel="stylesheet" href="./xModal.css">
+    <script src="./xModalV2.js"></script>
+```
+
+É necessário comentar/descomentar as linhas no arquivo xModal.js:
+
+```javascript
+    export default (function() { //comentar essa linha
+    let xModal = (function() { // descomentar essa linha
+```
+
+## Modo de Uso
+
+É necessário uma div no seu html com um id
+```html
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <title>Mofo v2.0</title>
+    </head>
+    <body>
+        <div id="minhaDiv">Hello World!</div>
+        <script src="index.js" type="module"></script>
+    </body>
+    </html>
+```
+No javascript iremos instanciar o objeto da seguinte forma:
+
+<h3>Create</h3>
+
+```javascript
+    const minhaDiv = new xModal.create({
+        el: '#minhaDiv'
+    })
+
+    //// Abre o modal
+    minhaDiv.open();
+```
+
+## Temas
+
+<table>
+    <tr>
+        <th>xModal-blue</th>
+        <th>xModal-opacity</th>
+        <th>xModal-dark</th>
+        <th>xModal-dark-square</th>
+    </tr>
+    <tr>
+        <td><img src="images/xModal.png"></td>
+        <td><img src="images/xModal-opacity.png"></td>
+        <td><img src="images/xModal-dark.png"></td>
+        <td><img src="images/xModal-dark-square.png"></td>
+    </tr>
+</table>
+
+## Parâmetro Create
+
+Create é responsável por instanciar o xModal. Recebe um objeto como parâmetro que pode ter as seguintes propriedades passadas:
+
+<table>
+    <tr>
+        <th align="center">Propriedade</th>
+        <th align="center">Descrição</th>
+        <th align="center">Tipo</th>
+        <th align="center">Default</th>
+    </tr>
+    <tr>
+        <td>el</td>
+        <td>Id do elemento (div) onde será instanciado o xModal. <strong>(Obrigatório)</strong></td>
+        <td align="center">String</td>
+        <td align="center"></td>
+    </tr>
+    <tr>
+        <td>title</td>
+        <td>Título da janela.</td>
+        <td align="center">String</td>
+        <td align="center">Mensagem do Sistema</td>
+    </tr>
+    <tr>
+        <td>width</td>
+        <td>Largura da janela.</td>
+        <td align="center">Numeric</td>
+        <td align="center">75%</td>
+    </tr>
+    <tr>
+        <td>height</td>
+        <td>Altura da janela.</td>
+        <td align="center">Numeric</td>
+        <td align="center">75%</td>
+    </tr>
+    <tr>
+        <td>resize</td>
+        <td>Propriedade que informa se poderá redimensionar a janela.</td>
+        <td align="center">Boolean</td>
+        <td align="center">False</td>
+    </tr>
+    <tr>
+        <td>theme</td>
+        <td>Tema que será utilizado na janela, temas disponíveis:<br>
+        xModal-blue, xModal-opacity, xModal-dark </td>
+        <td align="center">String</td>
+        <td align="center">xModal-blue</td>
+    </tr>
+    <tr>
+        <td>left</td>
+        <td>Define a posição (left) onde a janela será aberta.</td>
+        <td align="center">Numeric</td>
+        <td align="center"></td>
+    </tr>
+    <tr>
+        <td>top</td>
+        <td>Define a posição (top) onde a janela será aberta.</td>
+        <td align="center">Numeric</td>
+        <td align="center"></td>
+    </tr>
+    <tr>
+        <td>fullScreen</td>
+        <td>Caso queira que a janela abre em tela cheia, informe true.</td>
+        <td align="center">Boolean</td>
+        <td align="center">False</td>
+    </tr>
+    <tr>
+        <td>closeBtn</td>
+        <td>Mostra o botão fechar no canto superior direito da janela</td>
+        <td align="center">Boolean</td>
+        <td align="center">True</td>
+    </tr>
+    <tr>
+        <td>esc</td>
+        <td>Determina se o botão esc pode fechar o modal.</td>
+        <td align="center">Boolean</td>
+        <td align="center">True</td>
+    </tr>
+    <tr>
+        <td>modal</td>
+        <td>Limita o acesso apenas a janela aberta.</td>
+        <td align="center">Boolean</td>
+        <td align="center">True</td>
+    </tr>
+    <tr>
+        <td>titleDisplay</td>
+        <td>Possibilita a visualização ou não da barra de título da janela.</td>
+        <td align="center">Boolean</td>
+        <td align="center">True</td>
+    </tr>
+    <tr>
+        <td>onCreate</td>
+        <td>Função que será executada ao instanciar o objeto, só executa uma vez.</td>
+        <td align="center">Function</td>
+        <td align="center"></td>
+    </tr>
+    <tr>
+        <td>onOpen</td>
+        <td>Função que será executada sempre que abrir a janela.</td>
+        <td align="center">Function</td>
+        <td align="center"></td>
+    </tr>
+    <tr>
+        <td>onClose</td>
+        <td>Função que será executa sempre que fechar a janela.</td>
+        <td align="center">Function</td>
+        <td align="center"></td>
+    </tr>
+    <tr>
+        <td>onKeyDown</td>
+        <td>Permite a janela escutar pressionamento de teclas e executar uma função para as teclas determinadas.<pre>
+onKeyDown:{
+    13: (e)=>{console.log('enter pressionado')},    
+    'ctrl+13': (e)=>{console.log('ctrl+enter pressionado')}
+    'ctrl+shift+alt+13': (e)=>{
+        console.log('ctrl+shift+alt+13 pressionado')}
+    }</pre></td>
+        <td align="center">Object</td>
+        <td align="center"></td>
+    </tr>
+    <tr>
+        <td>buttons</td>
+        <td>Botões que irão aparecer na janela.<pre>
+buttons:{
+    btn1:  // identificador do botão
+    {
+        html: 'Confirma', //texto do botão
+        class: 'left',    // class do botão (opcional)
+        click: (e)=>{
+            // função que será executada ao 
+            clicar no botão
+        }   
+    },
+    btn2:{ // pode ser adicionado n botões }
+}
+        </pre></td>
+        <td align="center">Object</td>
+        <td align="center"></td>
+    </tr>
+    <tr>
+        <td>execAfter</td>
+        <td>Invoca o clique do botão após determinado tempo.
+        <pre>execAfter{
+            time: 10,           // tempo em segundos
+            btn: 'confirma'     // nome do btn
+        }</pre>
+        </td>
+        <td align="center">Object</td>
+        <td align="center"></td>
+    </tr>
+</table>
+
+## Métodos
+    
+Esses métodos serão utilizados na variável instanciada. Ex.:<br>
+```javascript
+    minhaDiv.close();
+```
+
+<table>
+    <tr>
+        <th>Método</th>
+        <th align="center">Parâmetro</th>
+        <th>Descrição</th>
+    <tr>
+    <tr>
+        <td>btnFocus</td>
+        <td align="center">nameBtn: String</td>
+        <td>Coloca o foco no botão passado por parâmetro.</td>
+    </tr>
+    <tr>
+        <td>btnDisable</td>
+        <td align="center">nameBtn: String</td>
+        <td>Desabilita o botão passado por parâmetro.</td>
+    </tr>
+    <tr>
+        <td>btnEnable</td>
+        <td align="center">nameBtn: String</td>
+        <td>Habilita o botão passado por parâmetro.</td>
+    </tr>
+    <tr>
+        <td>btnClick</td>
+        <td align="center">nameBtn: String</td>
+        <td>Clica no botão passado por parâmetro.</td>
+    </tr>
+    <tr>
+        <td>setTitle</td>
+        <td align="center">title: String</td>
+        <td>Altera o título da janela.</td>
+    </tr>
+    <tr>
+        <td>open</td>
+        <td align="center">nenhum</td>
+        <td>Abre a janela de diálogo.</td>
+    </tr>
+    <tr>
+        <td>close</td>
+        <td align="center">nenhum</td>
+        <td>Fecha a janela de diálogo.</td>
+    </tr>
+    <tr>
+        <td>destroy</td>
+        <td align="center">nenhum</td>
+        <td>Destrói a instância criada.</td>
+    </tr>
+</table>
+
+## Desenvolvedores
+
+[Francisco Alves](https://github.com/dffrancisco)<br>
+[Guilherme Trindade](https://github.com/guigagb)
---
+++ examples/example.js
@@ -0,0 +1,270 @@
+// import mofo from '../mofoV2.js';
+
+const exemplo1 = new mofo.create({
+    el: '#exemplo1',
+    resize: true,
+    // resize: {
+    //     topLeft: true,
+    //     buttonRight: true
+    // }
+});
+
+const exemplo2 = new mofo.create({
+    el: '#exemplo2',
+    title: 'Exemplo 2',
+    width: 400,
+    height: 200
+});
+
+const exemplo3 = new mofo.create({
+    el: '#exemplo3',
+    title: 'Exemplo 3',
+    left: 80,
+    top: 500
+});
+
+const exemplo4 = new mofo.create({
+    el: '#exemplo4',
+    title: 'Exemplo 4',
+    closeBtn: false
+});
+
+const exemplo5 = new mofo.create({
+    el: '#exemplo5',
+    title: 'Exemplo 5',
+    fullScreen: true
+});
+
+const exemplo6 = new mofo.create({
+    el: '#exemplo6',
+    title: 'Exemplo 6',
+    theme: 'mofo-dark'
+});
+
+const exemplo7 = new mofo.create({
+    el: '#exemplo7',
+    title: 'Exemplo 7',
+    buttons: {
+        cancelar: {
+            html: 'Cancelar',
+            class: 'left',
+            click: () => {
+                exemplo7.close();
+            }
+        },
+        confirmar: {
+            html: 'Confirmar',
+            class: 'right',
+            click: () => {
+                alert('Botão confirmar pressionado!');
+            }
+        }
+    }
+});
+
+const exemplo8 = new mofo.create({
+    el: '#exemplo8',
+    title: 'Exemplo 8',
+    onOpen: () => {
+        alert('Abrindo janela!');
+    },
+    onClose: () => {
+        alert('Fechando janela!');
+    }
+});
+
+const exemplo9 = new mofo.create({
+    el: '#exemplo9',
+    title: 'Exemplo 9',
+    execAfter: {
+        time: 15,
+        btn: 'fechar'
+    },
+    buttons: {
+        teste: {
+            html: 'teste',
+            click: () => { }
+        },
+        fechar: {
+            html: 'Fechar',
+            click: () => {
+                exemplo9.close();
+            }
+        }
+    }
+});
+
+const exemplo10 = new mofo.create({
+    el: '#exemplo10',
+    title: 'Exemplo 10',
+    onKeyDown: {
+        '13': (e) => { alert('Enter pressionado!') },
+        'ctrl+13': (e) => { alert('ctrl + enter pressionado!') },
+        'ctrl+shift+13': (e) => { alert('ctrl + shift + Enter pressionado!') },
+        'ctrl+shift+alt+13': (e) => { alert('ctrl + shift + alt + Enter pressionado!') },
+    }
+});
+
+function openExemplo1() {
+    exemplo1.open();
+}
+
+function openExemplo2() {
+    exemplo2.open();
+}
+
+function openExemplo3() {
+    exemplo3.open();
+}
+
+function openExemplo4() {
+    exemplo4.open();
+}
+
+function openExemplo5() {
+    exemplo5.open();
+}
+
+function openExemplo6() {
+    exemplo6.open();
+}
+
+function openExemplo7() {
+    exemplo7.open();
+}
+
+function openExemplo8() {
+    exemplo8.open();
+}
+
+function openExemplo9() {
+    exemplo9.open();
+}
+
+function openExemplo10() {
+    exemplo10.open();
+}
+
+// function makeResizableDiv(el, topLeft, topRight, buttonLeft, buttonRight) {
+//     const element = document.querySelector(el);
+
+//     const conteinerDiv = document.createElement('div')
+
+//     let constCss = 'width: 10px;height: 10px;border-radius: 50%;background: white;border: 3px solid #4286f4;position: absolute;'
+
+//     if (topLeft != false) {
+//         const topLeftDiv = document.createElement('div')
+//         topLeftDiv.classList.add('top-left', 'resizer')
+//         topLeftDiv.style = 'left: -5px;top: -5px;cursor: nwse-resize;' + constCss
+//         conteinerDiv.appendChild(topLeftDiv)
+//     }
+
+//     if (topRight != false) {
+//         const topRightDiv = document.createElement('div')
+//         topRightDiv.classList.add('top-right', 'resizer')
+//         topRightDiv.style = 'right: -5px;top: -5px;cursor: nesw-resize;' + constCss
+//         conteinerDiv.appendChild(topRightDiv)
+//     }
+
+//     if (buttonLeft != false) {
+//         const buttonLeftDiv = document.createElement('div')
+//         buttonLeftDiv.classList.add('bottom-left', 'resizer')
+//         buttonLeftDiv.style = 'left: -5px;bottom: -5px;cursor: nesw-resize;' + constCss
+//         conteinerDiv.appendChild(buttonLeftDiv)
+//     }
+
+//     if (buttonRight != false) {
+//         const buttonRightDiv = document.createElement('div')
+//         buttonRightDiv.classList.add('bottom-right', 'resizer')
+//         buttonRightDiv.style = 'right: -5px;bottom: -5px;cursor: nwse-resize;' + constCss
+//         conteinerDiv.appendChild(buttonRightDiv)
+//     }
+
+//     conteinerDiv.classList.add('resizers')
+//     element.appendChild(conteinerDiv)
+
+//     const resizers = conteinerDiv.querySelectorAll('.resizer')
+
+//     const minimum_size = 20;
+//     let original_width = 0;
+//     let original_height = 0;
+//     let original_x = 0;
+//     let original_y = 0;
+//     let original_mouse_x = 0;
+//     let original_mouse_y = 0;
+
+//     for (let i = 0; i < resizers.length; i++) {
+
+//         resizers[i].addEventListener('mousedown', function (e) {
+//             e.preventDefault()
+//             original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+//             original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+//             original_x = element.getBoundingClientRect().left;
+//             original_y = element.getBoundingClientRect().top;
+//             original_mouse_x = e.pageX;
+//             original_mouse_y = e.pageY;
+//             window.addEventListener('mousemove', resize)
+//             window.addEventListener('mouseup', stopResize)
+//         })
+
+//         function resize(e) {
+//             if (resizers[i].classList.contains('bottom-right')) {
+//                 const width = original_width + (e.pageX - original_mouse_x)
+//                 const height = original_height + (e.pageY - original_mouse_y)
+//                 if (width > minimum_size) element.style.width = width + 'px'
+//                 if (height > minimum_size) element.style.height = height + 'px'
+//             }
+//             else
+//                 if (resizers[i].classList.contains('bottom-left')) {
+//                     const height = original_height + (e.pageY - original_mouse_y)
+//                     const width = original_width - (e.pageX - original_mouse_x)
+//                     if (height > minimum_size) element.style.height = height + 'px'
+//                     if (width > minimum_size) {
+//                         element.style.width = width + 'px'
+//                         element.style.left = original_x + (e.pageX - original_mouse_x) + 'px'
+//                     }
+//                 }
+//                 else
+//                     if (resizers[i].classList.contains('top-right')) {
+//                         const width = original_width + (e.pageX - original_mouse_x)
+//                         const height = original_height - (e.pageY - original_mouse_y)
+//                         if (width > minimum_size) element.style.width = width + 'px'
+//                         if (height > minimum_size) {
+//                             element.style.height = height + 'px'
+//                             element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
+//                         }
+//                     }
+//                     else {
+//                         const width = original_width - (e.pageX - original_mouse_x)
+//                         const height = original_height - (e.pageY - original_mouse_y)
+//                         if (width > minimum_size) {
+//                             element.style.width = width + 'px'
+//                             element.style.left = original_x + (e.pageX - original_mouse_x) + 'px'
+//                         }
+//                         if (height > minimum_size) {
+//                             element.style.height = height + 'px'
+//                             element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
+//                         }
+//                     }
+//         }
+
+//         function stopResize() {
+//             window.removeEventListener('mousemove', resize)
+//         }
+//     }
+// }
+
+// makeResizableDiv('#exemplo1')
+
+
+
+window.openExemplo1 = openExemplo1;
+window.openExemplo2 = openExemplo2;
+window.openExemplo3 = openExemplo3;
+window.openExemplo4 = openExemplo4;
+window.openExemplo5 = openExemplo5;
+window.openExemplo6 = openExemplo6;
+window.openExemplo7 = openExemplo7;
+window.openExemplo8 = openExemplo8;
+window.openExemplo9 = openExemplo9;
+window.openExemplo10 = openExemplo10;
